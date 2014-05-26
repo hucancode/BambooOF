3f26d117750156079eeff853f893c8ec1df1ebd7
ngoaho91 <ngoaho91@gmail.com>
2014-05-26 21:12:41 +0700

    improve readability

--- addons/ofxSpriteRenderer/ofxSpriteCommand.h
+++ addons/ofxSpriteRenderer/ofxSpriteCommand.h
@@ -65,70 +65,81 @@ public:
 	void UpdateSprite(ofxSpriteQuad* sprite)
 	{
 		unsigned int index = sprite->m_IndexInCommand;
-		ofxVertex vertexA, vertexB, vertexC, vertexD;
+		ofxVertex *vertexA, *vertexB, *vertexC, *vertexD;
 		unsigned int index4 = index*4;
-		vertexA = m_Vertices[index4];
-		vertexB = m_Vertices[index4+1];
-		vertexC = m_Vertices[index4+2];
-		vertexD = m_Vertices[index4+3];
-		// TODO: using pointers instead of objects for 4 vertices
-		vertexA.X = sprite->GetPosition().x - sprite->GetWidth()*0.5;
-		vertexA.Y = sprite->GetPosition().y;
-		vertexA.Z = sprite->GetPosition().z;
-		vertexB.X = vertexA.X + sprite->GetWidth();
-		vertexB.Y = vertexA.Y;
-		vertexB.Z = vertexA.Z;
-		vertexC.X = vertexB.X;
-		vertexC.Y = vertexB.Y + sprite->GetHeight();
-		vertexC.Z = vertexA.Z;
-		vertexD.X = vertexA.X;
-		vertexD.Y = vertexC.Y;
-		vertexD.Z = vertexA.Z;
+		vertexA = &m_Vertices[index4];
+		vertexB = &m_Vertices[index4+1];
+		vertexC = &m_Vertices[index4+2];
+		vertexD = &m_Vertices[index4+3];
+
+		vertexA->X = sprite->GetPosition().x - sprite->GetWidth()*0.5;
+		vertexA->Y = sprite->GetPosition().y;
+		vertexA->Z = sprite->GetPosition().z;
+		vertexB->X = vertexA->X + sprite->GetWidth();
+		vertexB->Y = vertexA->Y;
+		vertexB->Z = vertexA->Z;
+		vertexC->X = vertexB->X;
+		vertexC->Y = vertexB->Y + sprite->GetHeight();
+		vertexC->Z = vertexA->Z;
+		vertexD->X = vertexA->X;
+		vertexD->Y = vertexC->Y;
+		vertexD->Z = vertexA->Z;
 		{
+			int tex_index = 0;
 			int i = 0;
-			int ix = 0;
-			int iy = 1;
+			int j = 1;
 			int x = 0;
 			int y = 1;
 			int w = 2;
 			int h = 3;
-			for(;i < sprite->GetMaterial()->GetTextureCount();
-				i++,
-				ix += 2, iy += 2, 
+			for(;tex_index < sprite->GetMaterial()->GetTextureCount();
+				tex_index++,
+				i += 2, j += 2, 
 				x += 4, y += 4, w += 4, h += 4)
 			{
-				int texture_w = sprite->GetMaterial()->GetTextureSizeRaw(ix);
-				int texture_h = sprite->GetMaterial()->GetTextureSizeRaw(iy);
+				float texture_w = sprite->GetMaterial()->GetTextureSizeRaw(i);
+				float texture_h = sprite->GetMaterial()->GetTextureSizeRaw(j);
+				float sprite_w = sprite->GetWidth();
+				float sprite_h = sprite->GetHeight();
+				float texture_rect_x = sprite->GetTextureRectRaw(x);
+				float texture_rect_y = sprite->GetTextureRectRaw(y);
+				float texture_rect_w = sprite->GetTextureRectRaw(w);
+				float texture_rect_h = sprite->GetTextureRectRaw(h);
+				float sprite_rect_x = sprite->GetSpriteRectRaw(x);
+				float sprite_rect_y = sprite->GetSpriteRectRaw(y);
+				float sprite_rect_w = sprite->GetSpriteRectRaw(w);
+				float sprite_rect_h = sprite->GetSpriteRectRaw(h);
 				{
-					float sprite_w = texture_w/(float)sprite->GetWidth();
-					float sprite_h = texture_h/(float)sprite->GetHeight();
-					float min_x = (sprite->GetTextureRectRaw(x) - sprite->GetSpriteRectRaw(x))/sprite_w;
-					float min_y = (sprite->GetTextureRectRaw(y) - sprite->GetSpriteRectRaw(y))/sprite_h;
-					float max_x = min_x + sprite_w;
-					float max_y = min_y + sprite_h;
-					vertexA.UV[ix] = min_x;
-					vertexA.UV[iy] = min_y;
-					vertexB.UV[ix] = max_x;
-					vertexB.UV[iy] = min_y;
-					vertexC.UV[ix] = max_x;
-					vertexC.UV[iy] = max_y;
-					vertexD.UV[ix] = min_x;
-					vertexD.UV[iy] = max_y;
-					//-------
-					float crop_w = sprite->GetWidth()/sprite->GetSpriteRectRaw(w);
-					float crop_h = sprite->GetHeight()/sprite->GetSpriteRectRaw(h);
-					float crop_min_x = -sprite->GetSpriteRectRaw(x)/sprite->GetSpriteRectRaw(w);
-					float crop_min_y = -sprite->GetSpriteRectRaw(y)/sprite->GetSpriteRectRaw(h);
+					float uv_w = texture_w/sprite_w;
+					float uv_h = texture_h/sprite_h;
+					float uv_min_x = (texture_rect_x - sprite_rect_x)/uv_w;
+					float uv_min_y = (texture_rect_y - sprite_rect_y)/uv_h;
+					float uv_max_x = uv_min_x + uv_w;
+					float uv_max_y = uv_min_y + uv_h;
+					vertexA->UV[i] = uv_min_x;
+					vertexA->UV[j] = uv_min_y;
+					vertexB->UV[i] = uv_max_x;
+					vertexB->UV[j] = uv_min_y;
+					vertexC->UV[i] = uv_max_x;
+					vertexC->UV[j] = uv_max_y;
+					vertexD->UV[i] = uv_min_x;
+					vertexD->UV[j] = uv_max_y;
+				}
+				{
+					float crop_w = sprite_w/sprite_rect_w;
+					float crop_h = sprite_h/sprite_rect_h;
+					float crop_min_x = -sprite_rect_x/sprite_rect_w;
+					float crop_min_y = -sprite_rect_y/sprite_rect_h;
 					float crop_max_x = crop_min_x + crop_w;
 					float crop_max_y = crop_min_y + crop_h;
-					vertexA.CUV[ix] = crop_min_x;
-					vertexA.CUV[iy] = crop_min_y;
-					vertexB.CUV[ix] = crop_max_x;
-					vertexB.CUV[iy] = crop_min_y;
-					vertexC.CUV[ix] = crop_max_x;
-					vertexC.CUV[iy] = crop_max_y;
-					vertexD.CUV[ix] = crop_min_x;
-					vertexD.CUV[iy] = crop_max_y;
+					vertexA->CUV[i] = crop_min_x;
+					vertexA->CUV[j] = crop_min_y;
+					vertexB->CUV[i] = crop_max_x;
+					vertexB->CUV[j] = crop_min_y;
+					vertexC->CUV[i] = crop_max_x;
+					vertexC->CUV[j] = crop_max_y;
+					vertexD->CUV[i] = crop_min_x;
+					vertexD->CUV[j] = crop_max_y;
 				}
 			}
 		}
