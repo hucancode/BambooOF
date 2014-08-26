265b0263fb91dac6382ff0c2f69dcbd5e3525c18
Nguyen Huu <ngoaho91@gmail.com>
2014-08-26 13:30:47 +0700

    roughly implement ofxTerrain
    build opengl data & render function

--- addons/ofxTerrain/ofxTerrain.cpp
+++ addons/ofxTerrain/ofxTerrain.cpp
@@ -1,12 +1,36 @@
 #include "ofxTerrain.h"
 ofxTerrain::ofxTerrain()
 {
-	glGenBuffers(1, &m_BaseVBOId);
-	glGenBuffers(1, &m_BaseIBOId);
-	glGenTextures(1, &m_BaseTextureId);
-	for(int i=0;i<NUMBER_OF_LAYERS;i++)
 	{
-		glGenTextures(1, &m_GroundTextureId[i]);
+		glGenBuffers(1, &m_BaseVBOId);
+		glGenBuffers(1, &m_BaseIBOId);
+		glGenTextures(1, &m_BaseTextureId);
+		for(int i=0;i<NUMBER_OF_LAYERS;i++)
+		{
+			glGenBuffers(1, &m_GroundVBOId[i]);
+			glGenBuffers(1, &m_GroundIBOId[i]);
+			glGenTextures(1, &m_GroundTextureId[i]);
+		}
+	}
+	{
+		m_VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+		const char* shader_source_raw = VERTEX_SHADER;
+		int shader_source_length = strlen(shader_source_raw)+1;
+		glShaderSource(m_VertexShaderId, 1, &shader_source_raw, &shader_source_length);
+		glCompileShader(m_VertexShaderId);
+	}
+	{
+		m_FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+		const char* shader_source_raw = FRAGMENT_SHADER;
+		int shader_source_length = strlen(shader_source_raw)+1;
+		glShaderSource(m_FragmentShaderId, 1, &shader_source_raw, &shader_source_length);
+		glCompileShader(m_FragmentShaderId);
+	}
+	{
+		m_ShaderProgramId = glCreateProgram();
+		glAttachShader(m_ShaderProgramId, m_VertexShaderId);
+		glAttachShader(m_ShaderProgramId, m_FragmentShaderId);
+		glLinkProgram(m_ShaderProgramId);
 	}
 }
 ofxTerrain::~ofxTerrain()
@@ -16,7 +40,15 @@ ofxTerrain::~ofxTerrain()
 	glDeleteTextures(1, &m_BaseTextureId);
 	for(int i=0;i<NUMBER_OF_LAYERS;i++)
 	{
+		glDeleteBuffers(1, &m_GroundVBOId[i]);
+		glDeleteBuffers(1, &m_GroundIBOId[i]);
 		glDeleteTextures(1, &m_GroundTextureId[i]);
+	}
+	{
+		glDetachShader(m_ShaderProgramId, m_FragmentShaderId);
+		glDeleteShader(m_FragmentShaderId);
+		glDetachShader(m_ShaderProgramId, m_VertexShaderId);
+		glDeleteShader(m_VertexShaderId);
 	}
 }
 void ofxTerrain::Initialize(int width, int height)
@@ -57,11 +89,89 @@ float ofxTerrain::GetHeight(int x, int y)
 }
 void ofxTerrain::BuildTileMap()
 {
-	// TODO: implement
+	{
+		// TODO: generate base indices & uv
+	}
+	for(int i=0;i<m_Width;i++)
+	{
+		for(int j=0;j<m_Height;j++)
+		{
+			if(m_TileMap[i][j] == INF) continue;
+			int layer = m_TileMap[i][j] / NUMBER_OF_DIFFERENT_TILE;
+			if(layer >= NUMBER_OF_LAYERS) continue;
+			int id = m_TileMap[i][j] % NUMBER_OF_DIFFERENT_TILE;
+
+			ofxTile vertex_a,
+				vertex_b,
+				vertex_c,
+				vertex_d;
+			// TODO: calculate uv
+			vertex_a.x = i*TILE_SIZE;
+			vertex_a.y = j*TILE_SIZE;
+			vertex_a.z = m_HeightMap[i][j];
+			vertex_b.x = vertex_a.x + TILE_SIZE;
+			vertex_b.y = vertex_a.y;
+			vertex_b.z = vertex_a.z;
+			vertex_c.x = vertex_b.x;
+			vertex_c.y = vertex_b.y + TILE_SIZE;
+			vertex_c.z = vertex_b.z;
+			vertex_d.x = vertex_a.x;
+			vertex_d.y = vertex_c.y;
+			vertex_d.z = vertex_c.z;
+			m_GroundIndices[layer].push_back(m_GroundVetices[layer].size()+0);
+			m_GroundIndices[layer].push_back(m_GroundVetices[layer].size()+1);
+			m_GroundIndices[layer].push_back(m_GroundVetices[layer].size()+2);
+			m_GroundIndices[layer].push_back(m_GroundVetices[layer].size()+0);
+			m_GroundIndices[layer].push_back(m_GroundVetices[layer].size()+2);
+			m_GroundIndices[layer].push_back(m_GroundVetices[layer].size()+3);
+			m_GroundVetices[layer].push_back(vertex_a);
+			m_GroundVetices[layer].push_back(vertex_b);
+			m_GroundVetices[layer].push_back(vertex_c);
+			m_GroundVetices[layer].push_back(vertex_d);
+		}
+	}
 }
 void ofxTerrain::RenderTiles()
 {
-	// TODO: implement
+	m_TransformMatrix = ofGetCurrentMatrix(OF_MATRIX_MODELVIEW)*ofGetCurrentMatrix(OF_MATRIX_PROJECTION);
+	glDepthMask(GL_FALSE);
+	glUseProgram(m_ShaderProgramId);
+	glUniform1i(m_ShaderLocationTexture,0);
+	glEnableVertexAttribArray(m_ShaderLocationXYZ);
+	glVertexAttribPointer(m_ShaderLocationXYZ, 3, GL_FLOAT, GL_FALSE, sizeof(ofxTile), (GLvoid*) offsetof( ofxTile, x));
+	glEnableVertexAttribArray(m_ShaderLocationUV);
+	glVertexAttribPointer(m_ShaderLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ofxTile), (GLvoid*) offsetof( ofxTile, u));
+	glUniformMatrix4fv(m_ShaderLocationTransform, 1, GL_FALSE, m_TransformMatrix.getPtr());
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_BaseVBOId);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_BaseIBOId);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(ofxTile)*4, &m_BaseVertices[0], GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*6, &m_BaseIndices[0], GL_STATIC_DRAW);
+		glBindTexture(GL_TEXTURE_2D, m_BaseTextureId);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glUseProgram(0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	for(int i=0;i<NUMBER_OF_LAYERS;i++)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_GroundVBOId[i]);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_GroundIBOId[i]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(ofxTile)*m_GroundVetices[i].size(), &m_GroundVetices[i][0], GL_DYNAMIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*m_GroundIndices[i].size(), &m_GroundIndices[i][0], GL_STATIC_DRAW);
+		glBindTexture(GL_TEXTURE_2D, m_GroundTextureId[i]);
+
+		glDrawElements(GL_TRIANGLES, m_GroundIndices[i].size(), GL_UNSIGNED_INT, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	glUseProgram(0);
+	glDepthMask(GL_TRUE);
 }
 bool ofxTerrain::LoadBaseTexture(string path)
 {
--- addons/ofxTerrain/ofxTerrain.h
+++ addons/ofxTerrain/ofxTerrain.h
@@ -4,17 +4,49 @@
 #include "FreeImage.h"
 #define INF 10000
 #define NUMBER_OF_LAYERS 3
+#define NUMBER_OF_DIFFERENT_TILE 16
+#define TILE_SIZE 64
+#define VERTEX_SHADER											\
+"uniform mat4 u_transform_matrix;								\
+attribute vec3 a_position;										\
+attribute vec2 a_uv;											\
+varying vec2 v_uv;												\
+void main()														\
+{																\
+	v_uv = a_uv;												\
+	gl_Position = u_transform_matrix*vec4(a_position,1.0);		\
+}"
+#define FRAGMENT_SHADER											\
+"uniform sampler2D u_texture;									\
+varying vec2 v_uv;												\
+void main()														\
+{																\
+	vec4 color = texture2D(u_texture,v_uv);						\
+    gl_FragColor = color;										\
+}"
 
 class ofxTerrain
 {
+private:
+	ofMatrix4x4				m_TransformMatrix;
+	GLuint					m_ShaderProgramId;
+	GLuint					m_VertexShaderId;
+	GLuint					m_FragmentShaderId;
+	GLint					m_ShaderLocationXYZ;
+	GLint					m_ShaderLocationUV;
+	GLint					m_ShaderLocationTexture;
+	GLint					m_ShaderLocationTransform;
 private:
 	int						m_Width;
 	int						m_Height;
 	vector<vector<short> >	m_TileMap;
 	vector<vector<float> >	m_HeightMap;
 private:
+	ofxTile					m_BaseVertices[4];
 	GLuint					m_BaseVBOId;
+	GLuint					m_BaseIndices[4];
 	GLuint					m_BaseIBOId;
+	
 	vector<ofxTile>			m_GroundVetices[NUMBER_OF_LAYERS];
 	GLuint					m_GroundVBOId[NUMBER_OF_LAYERS];
 	vector<GLuint>			m_GroundIndices[NUMBER_OF_LAYERS];
