77f1c874893766246a88e9e46d9474dd6a004b60
ngoaho91 <ngoaho91@gmail.com>
2014-09-13 14:32:10 +0700

    implementing particle effect 2d

--- addons/ofxSpriteRenderer/ofxParticleEffect2D.cpp
+++ addons/ofxSpriteRenderer/ofxParticleEffect2D.cpp
@@ -13,6 +13,52 @@ void ofxParticleEffect2D::AddEmitter(ofxEmitter2D* emitter)
 }
 void ofxParticleEffect2D::Update(float delta_time)
 {
+	for(int i = 0;i < m_Emitters.size();i++)
+	{
+		ofxEmitter2D* e = m_Emitters[i];
+		e->cooldown -= delta_time;
+		if(e->cooldown <= 0)
+		{
+			int particle_to_emit = e->emission_rate + ofRandom(e->emission_rate_var);
+			int j = m_ParticleCount;
+			m_ParticleCount += particle_to_emit;
+			for(;j<m_ParticleCount;j++)
+			{
+				m_ParticlePool[j].emitter = e;
+				m_ParticlePool[j].life = e->life + ofRandom(e->life_var);
+				m_ParticlePool[j].speed = e->speed + ofRandom(e->speed_var);
+				m_ParticlePool[j].radial_accel = e->radial_accel + ofRandom(e->radial_accel_var);
+				m_ParticlePool[j].tangental_accel = e->tangental_accel + ofRandom(e->tangental_accel_var);
+				// TODO: calculate these below
+				m_ParticlePool[j].position;
+				m_ParticlePool[j].vertices[0];
+				m_ParticlePool[j].vertices[1];
+				m_ParticlePool[j].vertices[2];
+				m_ParticlePool[j].vertices[3];
+			}
+			e->cooldown = e->emission_time + ofRandom(e->emission_time_var);
+		}
+	}
+	for(int i=0;i<m_ParticleCount;i++)
+	{
+		ofxParticle2D& item = m_ParticlePool[i];
+		item.life -= delta_time;
+		if(item.life <= 0 || item.vertices[0].opacity <= 0)// life
+		{
+			// TODO: do kill particle
+		}
+		{// force
+			float radial = item.position.angleRad(item.emitter->position);
+			float tangental = radial + HALF_PI;
+			ofVec2f radial_force = item.speed*ofVec2f(1,1).rotateRad(radial);
+			ofVec2f tangental_force = item.speed*ofVec2f(1,1).rotateRad(tangental);
+			item.position += radial_force;
+			item.position += tangental_force;
+		}
+		{// TODO: appearance
+
+		}
+	}
 }
 void ofxParticleEffect2D::SubmitChanges()
 {
--- addons/ofxSpriteRenderer/ofxParticleEffect2D.h
+++ addons/ofxSpriteRenderer/ofxParticleEffect2D.h
@@ -7,16 +7,6 @@ note: this effect has all of it's particle stick to a plane
 
 const int MAX_PARTICLE2D_COUNT = 200;
 
-struct ofxParticle2D
-{
-	ofVec2f		position;
-	float		life;
-	float		speed;
-	float		angle;
-	float		radial_accel;
-	float		tangental_accel;
-	ofxVertex	vertices[4];
-};
 struct ofxEmitter2D
 {
 	ofVec2f		position;
@@ -24,6 +14,7 @@ struct ofxEmitter2D
 	int			emission_rate_var;
 	float		emission_time;
 	float		emission_time_var;
+	float		cooldown;
 	float		life;
 	float		life_var;
 	float		radius;
@@ -49,6 +40,18 @@ struct ofxEmitter2D
 	ofColor		end_color;
 	ofColor		end_color_var;
 };
+
+struct ofxParticle2D
+{
+	ofxEmitter2D*	emitter;
+	ofVec2f			position;
+	float			life;
+	float			speed;
+	float			radial_accel;
+	float			tangental_accel;
+	ofxVertex		vertices[4];
+};
+
 typedef vector<ofxEmitter2D*> ofxEmitter2Ds;
 class ofxParticleEffect2D
 	:public ofxSpriteBase
@@ -66,4 +69,6 @@ public:
 	virtual void			SubmitChanges();
 	void					PauseResume();
 	bool					IsPaused();
+private:
+	void					KillParticle(int index);
 };
