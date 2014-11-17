959f686d4859ccc8b7e787c5e480529e57503d27
Nguyen Huu <ngoaho91@gmail.com>
2014-11-17 22:22:02 +0700

    ofxShaderProgram now very flexible

--- addons/ofxSpriteRenderer/ofxShaderProgram.h
+++ addons/ofxSpriteRenderer/ofxShaderProgram.h
@@ -20,6 +20,8 @@ public:
 	ofxShaderProgram();
 	virtual ~ofxShaderProgram();
 	virtual bool		Load(string program_file);
+	virtual void		BuildReferenceMap();
+	virtual void		BindReferenceMap();
 	virtual void		IncreaseReference();
 	virtual void		DecreaseReference();
 	bool				Load(string vs_file, string fs_file);
