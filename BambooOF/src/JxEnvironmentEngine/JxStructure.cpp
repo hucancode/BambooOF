570db7f4d7bc757361edaddc8a60907ff726d94c
Nguyen Huu <ngoaho91@gmail.com>
2015-01-11 20:58:04 +0700

    generate structure model

--- BambooOF/src/JxEnvironmentEngine/JxStructure.cpp
+++ BambooOF/src/JxEnvironmentEngine/JxStructure.cpp
@@ -1,1 +1,37 @@
 #include "JxStructure.h"
+JxStructure::JxStructure()
+{
+}
+JxStructure::~JxStructure()
+{
+}
+bool JxStructure::Load(string xml_file)
+{
+}
+void JxStructure::BuildModel()
+{
+	{// projecting
+		vector<ofVec2f>::iterator it = m_BasePolygon.begin();
+		for (; it != m_BasePolygon.end(); it++)
+		{
+			ofVec2f point = *it;
+			ofVec3f projection;
+			projection.x = point.x;
+			projection.y = 0;
+			projection.z = point.y / cos(PI*0.125);
+			m_Model.push_back(projection);
+		}
+	}
+	{// extending
+		vector<ofVec3f>::iterator it = m_Model.begin();
+		for (; it != m_Model.end(); it++)
+		{
+			ofVec3f point = *it;
+			ofVec3f extend;
+			extend.x = point.x;
+			extend.y = m_Height;
+			extend.z = point.z;
+			m_Model.push_back(extend);
+		}
+	}
+}
--- BambooOF/src/JxEnvironmentEngine/JxStructure.h
+++ BambooOF/src/JxEnvironmentEngine/JxStructure.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "ofMain.h"
+#include "ofxSpriteQuad.h"
+
+class JxStructure : public ofxSpriteQuad, public ofxResource
+{
+private:
+	vector<ofVec2f> m_BasePolygon;
+	vector<ofVec3f> m_Model;
+	float m_Height;
+public:
+	JxStructure();
+	~JxStructure();
+	virtual bool Load(string xml_file);
+private:
+	void BuildModel();
+};
